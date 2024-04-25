0676af94393f2f0bb1a1acc0f53fcbc902d680ae
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2024-04-25 16:03:55 -0300

    :lipstick: feat: Alteração de formato da String mensagem

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,34 +1,48 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Por favor, digite o numero da Conta: ");
-        int numero = sc.nextInt();
-        System.out.println();
+        boolean inputValido = false;
+        int numero = 0;
+        String agencia = "";
+        String nome = "";
+        double saldo = 0;
 
-        System.out.println("Por favor, digite o numero da Agencia: ");
-        String agencia = sc.next();
-        sc.nextLine();
+        do {
+            try {
+                System.out.println("Por favor, digite o numero da Conta: ");
+                numero = sc.nextInt();
+                sc.nextLine();
 
-        System.out.println("Por favor, digite o nome do Cliente: ");
-        String nome = sc.nextLine();
-        sc.nextLine();
+                System.out.println("\nPor favor, digite o numero da Agencia: ");
+                agencia = sc.next();
+                sc.nextLine();
 
-        System.out.println("Por favor, digite o Saldo: ");
-        double saldo = sc.nextDouble();
-        System.out.println();
+                System.out.println("\nPor favor, digite o nome do Cliente: ");
+                nome = sc.nextLine();
+                sc.nextLine();
 
-        System.out.println("" +
-                "Olá " + nome +
-                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
-        ", conta " + numero +
-        " e seu saldo " + saldo +
-        " já está disponível para saque."
-        );
+                System.out.println("Por favor, digite o Saldo: ");
+                saldo = sc.nextDouble();
+                sc.nextLine();
 
+                inputValido = true;
 
+            } catch (InputMismatchException e) {
+                System.err.println("Erro ao ler dados. Por favor, tente novamente.");
+                sc.next();
+            }
+        } while (!inputValido);
+        sc.close();
+        String mensagem = String.format("\n" +
+                "Olá, %s!\n" +
+                "Obrigado por criar uma conta em nosso banco.\n" +
+                "Sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.\n", nome, agencia, numero, saldo);
+
+        System.out.println(mensagem);
     }
 
-}
+    }
